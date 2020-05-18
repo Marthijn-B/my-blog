59784069328347c3506ec8b0a977d686c02177f0
Marthijn Batlajeri <marthijn.batlajeri@marthijnbnb.objective.com>
2020-05-19 08:30:18 +1200

    Using MongoDB now

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch //will ensure that only one Route will be actioned
 } from 'react-router-dom';
 
 // stylesheets
 import './App.css';
 
-// components
+// pages
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleListPage from './pages/ArticleListPage';
 import ArticlePage from './pages/ArticlePage';
+import NotFoundPage from './pages/NotFoundPage';
+
+// components
 import NavBar from './navbar';
 
 function App() {
@@ -20,10 +24,13 @@ function App() {
       <div className="App">
         <NavBar />
         <div id="page-body">
-          <Route path="/" component={HomePage} exact/>
-          <Route path="/about" component={AboutPage} exact/>
-          <Route path="/article-list" component={ArticleListPage} exact/>
-          <Route path="/article/:name" component={ArticlePage} exact/>
+          <Switch>
+            <Route path="/" component={HomePage} exact/>
+            <Route path="/about" component={AboutPage} exact/>
+            <Route path="/article-list" component={ArticleListPage} exact/>
+            <Route path="/article/:name" component={ArticlePage} exact/>
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 // article data
 import articleContent from './article-content';
+// components
+import ArticleList from '../components/ArticleList';
+// pages
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
 
-  if(!article) { return <h1>Article does not exist</h1> }
+  if(!article) { return <NotFoundPage /> };
+
+  const otherArticles = articleContent.filter(article => article.name !== name);
 
   return (
     <>
@@ -16,7 +22,8 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph,key) => (
         <p key={key}>{paragraph}</p>
       )) }
-
+      <h3>Related Articles:</h3>
+      <ArticleList articles={otherArticles} />
 
     </>
   )
