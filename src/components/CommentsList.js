1192f8a216e98abb348ec5bcfceb8b39197b9ed7
Marthijn Batlajeri <marthijn.batlajeri@marthijnbnb.objective.com>
2020-05-19 20:36:22 +1200

    Added comments section/component

---
+++ src/components/CommentsList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CommentsList = ({ comments }) => (
+  <>
+    {comments.map((comment, key) => (
+      <>
+      <h3>Comments</h3>
+        <div className="comment" key={key}>
+          <h4>{comment.username}</h4>
+          <p>{comment.text}</p>
+        </div>
+      </>
+    ))}
+  </>
+);
+
+export default CommentsList;
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 // article data
 import articleContent from './article-content';
 // components
 import ArticleList from '../components/ArticleList';
+import CommentsList from '../components/CommentsList';
 // pages
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState ({ upvotes: 0, comments : [] });
+
+  // The following will only execute useEffect when it loads
+  // useEffect(() => { setSomething }, [] )
+
+  // execute useEffect when [name] changes
+  useEffect(() => {
+    // fetch current data function
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log(body);
+      setArticleInfo(body);
+    }
+    fetchData();
+  }, [name] );
 
   if(!article) { return <NotFoundPage /> };
 
@@ -19,12 +37,15 @@ const ArticlePage = ({ match }) => {
     <>
     {/* <> equal to <React.fragment> to bundle all elements to export */}
       <h1>{article.title}</h1>
+      <p>This article has been upvoted {articleInfo.upvote} times</p>
       {article.content.map((paragraph,key) => (
         <p key={key}>{paragraph}</p>
       )) }
+      { articleInfo.comments &&
+        <CommentsList comments={articleInfo.comments} />
+      }
       <h3>Related Articles:</h3>
       <ArticleList articles={otherArticles} />
-
     </>
   )
 }
